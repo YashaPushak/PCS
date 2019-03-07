870640ee419e9cc52e8a3a7470fa7a283be2f982
Yasha Pushak <ypushak@ada.cs.ubc.ca>
2019-03-07 11:26:26 -0800

    Second test added. Can succesfully parse and print two documents in one python session.

--- PCS/examples.py
+++ PCS/examples.py
@@ -4,3 +4,8 @@ pcs = pcsParser.PCS('../examples/params-lkh.pcs')
 
 with open('../examples/params-lkh-copy.pcs','w') as f_out:
     f_out.write(pcs.printDocument())
+
+pcs2 = pcsParser.PCS('../examples/params-cplex.pcs')
+
+with open('../examples/params-cplex-copy.pcs','w') as f_out:
+    f_out.write(pcs2.printDocument())
